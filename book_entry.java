01f86c7827f280ca4a0e5be138967f33cb8a6ff6
Jakey1610 <mortimer907@gmail.com>
2016-09-09 08:37:53 +0100

    added user input

--- book_entry.java
+++ book_entry.java
@@ -1,5 +1,9 @@
+import java.util.*;
 public class book_entry{
     public static void main(String[] args){
-            System.out.println("Hello World!");
+            Scanner reader = new Scanner(System.in);
+    		System.out.println("Please enter a book name: ");
+            String book = reader.nextLine();
+            System.out.println(book);
         }
 }
